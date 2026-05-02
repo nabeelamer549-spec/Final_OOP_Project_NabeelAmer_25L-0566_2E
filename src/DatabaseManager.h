b993e1f460c9bc1e65f8b72442c66ead5a80be23
nabeelamer549-spec <nabeelamer549@gmail.com>
2026-05-02 20:58:20 +0500

    Added all txt files and created DatabaseManager class

---
+++ src/DatabaseManager.h
@@ -0,0 +1,24 @@
+#ifndef DATABASEMANAGER_H
+#define DATABASEMANAGER_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class DatabaseManager
+{
+    string str;
+
+public:
+    DatabaseManager()
+    {
+    }
+    void readfile(string fname)
+    {
+    }
+    void writefile(string fname, string str)
+    {
+    }
+};
+
+#endif
